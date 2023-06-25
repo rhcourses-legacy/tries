f17443e19246da7dfbf24c8ce4f3f7b8cd9eda87
Reiner Hüchting <dev@reinerhuechting.de>
2023-06-25 12:40:01 +0200

    Add a data field to trie nodes.

--- tries/node.go
+++ tries/node.go
@@ -1,9 +1,11 @@
 package tries
 
 // Node is a trie node.
-// It contains a map of children nodes.
+// It contains data and a map of children nodes.
+//   - data is a slice of strings because a node's data in our examples is a list of words.
 //   - children is a map of runes to nodes because we traverse the trie by reading characters from a string.
 type Node struct {
+	data     []string
 	children map[rune]*Node
 }
 
@@ -17,4 +19,9 @@ func NewNode() *Node {
 // IsLeaf returns true if the node is a leaf.
 func (n *Node) IsLeaf() bool {
 	return len(n.children) == 0
+}
+
+// IsEmpty returns true if the node is empty.
+func (n *Node) IsEmpty() bool {
+	return len(n.data) == 0
 }
--- tries/node_test.go
+++ tries/node_test.go
@@ -10,3 +10,12 @@ func TestNode_New_IsLeaf(t *testing.T) {
 		t.Error("New node should be a leaf")
 	}
 }
+
+// TestNode_New_IsEmpty tests the NewNode function.
+// It creates a new node and checks whether it is empty.
+func TestNode_New_IsEmpty(t *testing.T) {
+	n := NewNode()
+	if !n.IsEmpty() {
+		t.Error("New node should be empty")
+	}
+}
